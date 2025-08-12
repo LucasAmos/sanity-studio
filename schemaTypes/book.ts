d607f8a71b8b8dbfb29c804ca9836023db4cf34d
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2025-08-12 14:50:53 +0100

    SANITY_STUDIO_DATASET

---
+++ schemaTypes/book.ts
@@ -0,0 +1,19 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'book',
+  title: 'Book',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'string',
+    }),
+  ],
+})
