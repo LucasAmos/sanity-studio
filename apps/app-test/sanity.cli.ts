37b197983ba5dab88bb7be0e49e08d8b32fa7ace
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2025-08-13 14:37:51 +0100

    app 2

---
+++ apps/app-test/sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  app: {
+    organizationId: "oRo3CJogR",
+    entry: "./src/App.tsx",
+    id: "q8lp1kdbj0j6ry9g9enls492",
+  },
+});
