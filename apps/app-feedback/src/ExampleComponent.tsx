dd8bced8325928c058538f1caf3c679bcb42683f
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2025-08-13 11:49:00 +0100

    wip

--- apps/app-feedback/src/ExampleComponent.tsx
+++ apps/app-feedback/src/ExampleComponent.tsx
@@ -1,8 +1,16 @@
-import {type CurrentUser, useCurrentUser} from '@sanity/sdk-react'
-import {Avatar, Card, Container, Flex, Heading, Stack, Text} from '@sanity/ui'
+import { type CurrentUser, useCurrentUser } from "@sanity/sdk-react";
+import {
+  Avatar,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+} from "@sanity/ui";
 
 export function ExampleComponent() {
-  const user: CurrentUser | null = useCurrentUser()
+  const user: CurrentUser | null = useCurrentUser();
 
   return (
     <Container width={1}>
@@ -12,22 +20,27 @@ export function ExampleComponent() {
           <Heading as="h1">Welcome to your Sanity App, {user?.name}!</Heading>
           <Stack space={4}>
             <Text muted>
-              This is an example component, rendered with Sanity UI and the{' '}
-              <code>useCurrentUser</code> hook from the Sanity App SDK. You can import and use any
-              Sanity UI components and App SDK hooks anywhere in this application.
+              This is an example component 3, rendered with Sanity UI and the{" "}
+              <code>useCurrentUser</code> hook from the Sanity App SDK. You can
+              import and use any Sanity UI components and App SDK hooks anywhere
+              in this application.
             </Text>
             <Text muted>
-              You can also replace this component with components of your own. Render them in your
-              app by importing and using them in your application’s <code>src/App.tsx|jsx</code>{' '}
-              file.
+              You can also replace this component with components of your own.
+              Render them in your app by importing and using them in your
+              application’s <code>src/App.tsx|jsx</code> file.
             </Text>
             <Text muted>
-              Looking for more guidance? See the <a href="https://sanity.io/ui">Sanity UI docs</a>{' '}
-              and the <a href="https://reference.sanity.io/_sanity/sdk-react/">Sanity App SDK docs</a>!
+              Looking for more guidance? See the{" "}
+              <a href="https://sanity.io/ui">Sanity UI docs</a> and the{" "}
+              <a href="https://reference.sanity.io/_sanity/sdk-react/">
+                Sanity App SDK docs
+              </a>
+              !
             </Text>
           </Stack>
         </Flex>
       </Card>
     </Container>
-  )
+  );
 }
