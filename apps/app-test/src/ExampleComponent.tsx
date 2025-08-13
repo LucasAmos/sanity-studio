a71819f81a72c0a3166d86961ccd8568c85d338c
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2025-08-13 14:53:06 +0100

    wip

--- apps/app-test/src/ExampleComponent.tsx
+++ apps/app-test/src/ExampleComponent.tsx
@@ -17,7 +17,9 @@ export function ExampleComponent() {
       <Card padding={5} shadow={3} radius={3} marginY={5}>
         <Flex align="center" direction="column" gap={5} marginY={4}>
           <Avatar size={3} src={user?.profileImage} />
-          <Heading as="h1">Welcome to your Sanity App, {user?.name}!</Heading>
+          <Heading as="h1">
+            Welcome to your Sanity App test-2, {user?.name}!
+          </Heading>
           <Stack space={4}>
             <Text muted>
               This is an example component, rendered with Sanity UI and the{" "}
