3a6c333560b3cbc72bbe875545a8bb36d3c4695a
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2025-09-05 12:52:17 +0100

    author type

--- studio/schemaTypes/book.ts
+++ studio/schemaTypes/book.ts
@@ -14,7 +14,8 @@ export default defineType({
     defineField({
       name: 'author',
       title: 'Author',
-      type: 'string',
+      type: 'reference',
+      to: [{type: 'author'}],
       validation: (rule) => rule.required(),
     }),
     defineField({
