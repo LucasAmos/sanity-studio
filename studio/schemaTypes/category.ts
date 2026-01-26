43876e984d0b253c0d4058a9203a16f40faac4f1
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2026-01-26 16:41:53 +0000

    feat(schema): add rewrite type

--- studio/schemaTypes/category.ts
+++ studio/schemaTypes/category.ts
@@ -1,4 +1,5 @@
 import {defineField, defineType} from 'sanity'
+import groq from 'groq'
 
 export default defineType({
   name: 'category',
@@ -20,5 +21,46 @@ export default defineType({
       },
       validation: (rule) => rule.required(),
     },
+    defineField({
+      name: 'rewrite',
+      title: 'Rewrite',
+      type: 'reference',
+      to: [{type: 'rewrite'}],
+      validation: (Rule) =>
+        Rule.custom(async (value, context) => {
+          console.log('***1', value)
+          console.log('***2', context)
+
+          if (!value || !value._ref) {
+            console.log('No rewrite selected, so no validation needed')
+            return true
+          }
+          const client = context.getClient({apiVersion: '2021-03-25'})
+          console.log('value._ref: ', value._ref)
+          const res = await client.fetch(
+            groq`*[_type != "rewrite" && references($rewriteId)]{...}`,
+            {
+              rewriteId: value._ref,
+            },
+            {
+              perspective: 'published',
+            },
+          )
+          console.log('**res', res)
+
+          if (res[0]?._id === context.document?._id) {
+            // if the reference of document linked to the rewrite is the same as
+            // this document then that is allowed
+            return true
+          }
+
+          // this can probably be removed and just return false
+
+          if (res.length > 0) {
+            return {message: 'Slug can only be use once'}
+          }
+          return true
+        }),
+    }),
   ],
 })
