30c20dd4bd56f2eae67fbee8f5fbaf04325719c4
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2026-02-14 19:10:16 +0000

    feat(studio): author validation

--- studio/schemaTypes/author.ts
+++ studio/schemaTypes/author.ts
@@ -1,4 +1,33 @@
-import {defineField, defineType} from 'sanity'
+import {defineField, defineType, StringRule} from 'sanity'
+import groq from 'groq'
+
+export const uniqueValidation = (rule: StringRule) => {
+  return rule.custom(async (value, context) => {
+    const client = context.getClient({apiVersion: '2021-03-25'})
+    const type = context.document!._type
+
+    const res = await client.fetch(
+      groq`*[ _type == $type && name == $name] {_id}`,
+      {
+        type: type,
+        name: value,
+      },
+      {
+        perspective: 'published',
+      },
+    )
+
+    if (res[0]?._id === context.document?._id) {
+      // if the reference of document linked to the rewrite is the same as
+      // this document then that is allowed
+      return true
+    }
+    if (res.length === 0) {
+      return true
+    }
+    return {message: 'An author already exists with that name'}
+  })
+}
 
 export default defineType({
   name: 'author',
@@ -9,7 +38,7 @@ export default defineType({
       name: 'name',
       title: 'Name',
       type: 'string',
-      validation: (rule) => rule.required(),
+      validation: uniqueValidation,
     }),
   ],
 })
