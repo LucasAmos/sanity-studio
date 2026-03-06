9792ebe297a575f193f7961ca88ad367606ef036
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2026-03-06 14:23:02 +0000

    feat(rewrite): remove rewrite

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -2,6 +2,5 @@ import alias from './alias'
 import book from './book'
 import category from './category'
 import author from './author'
-import rewrite from './rewrite'
 import redirect from './redirect'
-export const schemaTypes = [alias, author, book, category, redirect, rewrite]
+export const schemaTypes = [alias, author, book, category, redirect]
