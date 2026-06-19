759839bb2192402ebaea76d9d9d0ff9eee47940d
Lucas Amos <9055266+LucasAmos@users.noreply.github.com>
2026-06-19 17:57:24 +0100

    feat(locations): define locations

--- studio/presentation/resolve.ts
+++ studio/presentation/resolve.ts
@@ -1,13 +1,33 @@
 import { defineLocations, PresentationPluginOptions } from "sanity/presentation";
 
+import book from "../schemaTypes/book";
+import about from "../schemaTypes/pages/about";
+import cv from "../schemaTypes/pages/cv";
+
 export const resolve: PresentationPluginOptions["resolve"] = {
   locations: {
-    book: defineLocations({
+    [book.name]: defineLocations({
       select: {
         title: "title"
       },
       resolve: (doc) => ({
         locations: [{ title: doc?.title || "Untitled", href: "/books" }]
+      })
+    }),
+    [about.name]: defineLocations({
+      select: {
+        title: "title"
+      },
+      resolve: () => ({
+        locations: [{ title: "About", href: "/about" }]
+      })
+    }),
+    [cv.name]: defineLocations({
+      select: {
+        title: "title"
+      },
+      resolve: () => ({
+        locations: [{ title: cv.title, href: "/about/cv" }]
       })
     })
   }
